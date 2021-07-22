87f10f158926acb8b4976d56d0a1cc867e9c2c12
likai <1774574746@qq.com>
2021-07-22 22:07:48 +0800

    7月22号更新

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    用户列表
+    <el-row :gutter="5">
+      <el-col :span="2" class="keyWord">关键字:</el-col>
+      <el-col :span="3">
+        <el-select></el-select>
+      </el-col>
+      <el-col :span="5">
+        <el-input></el-input>
+      </el-col>
+      <el-col :span="10">
+        <el-button type="danger">搜索</el-button>
+      </el-col>
+      <el-col :span="4">
+        <el-button type="danger" class="pull-right">添加用户</el-button>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -11,4 +25,9 @@ export default {
 </script>
 
 <style scoped lang="less">
+.keyWord{
+  height: 40px;
+  line-height: 40px;
+  font-size: 17px;
+}
 </style>
